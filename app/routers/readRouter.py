b5fbf52d117cf7e18b2f4db23d97eedfbc490033
leftyy <ustyantsev.makar@mail.ru>
2024-04-21 01:50:33 +0300

    added GET method for SELECT *

--- app/routers/readRouter.py
+++ app/routers/readRouter.py
@@ -4,6 +4,8 @@ import app.database.database as db
 
 
 read_router = APIRouter()
+
+
 http_handler = error.HttpErrorHandler(__name__)
 db_handler = error.DBErrorHandler(__name__)
 
@@ -22,3 +24,8 @@ async def get_user(user_id: int) -> dict[str, list[str] | int | None]:
     
     else:
         http_handler.handle_http_err(status=404, msg="User data not found")
+
+
+@read_router.get("/get_all_users")
+async def select_all():
+    return db.select_all()
